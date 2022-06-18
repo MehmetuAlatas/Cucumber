572514da5f8447e908dfe39025761c48cc47d9b3
Mehmet Alatas <mehmetualatas@gmail.com>
2022-06-18 20:33:11 +0300

    u

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -1,22 +1,20 @@
 package runners;
-
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
-
 @RunWith(Cucumber.class)
 @CucumberOptions(
-
+        //
         plugin = {
-                "html:target/default-cucumber-reports.html",
+                "pretty",
+                "html:target/default_cucumber_report.html",
                 "json:target/json-reports/cucumber.json",
                 "junit:target/xml-report/cucumber.xml",
-                "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"
+                "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
         },
-
         features = "./src/test/resources/features",//path od features folder
         glue = {"stepdefinitions","hooks"},//path of the step definitions folder
-        tags = "@customer_login",
+        tags = "@excel_feature",
         dryRun = false
 )
 public class Runner {
